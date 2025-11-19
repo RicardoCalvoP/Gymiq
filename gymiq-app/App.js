7f8f2d5697fd76262cd25d97c95eea4d5b45ad7e
RicardoCalvoP <a01028889@tec.mx>
2025-11-18 22:52:37 -0600

    feat: minor changes

--- gymiq-app/App.js
+++ gymiq-app/App.js
@@ -2,17 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import Main  from './app/components/Main.jsx';
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { UserProvider } from './app/context/UserContext';
 
 
 export default function App() {
   return (
-        <SafeAreaProvider>
+      <SafeAreaProvider>
         <View className="flex-1 items-center justify-center">
-            <StatusBar style="light" />
-            <Main />
-          </View>
-        </SafeAreaProvider>
-
-
+          <StatusBar style="light" />
+          <Main />
+        </View>
+      </SafeAreaProvider>
   );
 }
