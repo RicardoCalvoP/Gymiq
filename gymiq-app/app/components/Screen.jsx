2f9834b1bb892e667407941a82af5f0055a4091d
RicardoCalvoP <a01028889@tec.mx>
2025-11-25 15:08:24 -0600

    feat: save area view

--- gymiq-app/app/components/Screen.jsx
+++ gymiq-app/app/components/Screen.jsx
@@ -1,9 +1,9 @@
-import { View} from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Screen({ children}) {
+export default function Screen({ children }) {
   return(
-    <View className="bg-[#000] flex-1 width-100% px-2">
+    <SafeAreaView className="bg-[#000] flex-1 width-100% px-2">
         {children}
-    </View>
+    </SafeAreaView>
   );
 }
