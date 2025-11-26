73be02e67cdcf4c7adc73424e7171612e0950e7f
RicardoCalvoP <a01028889@tec.mx>
2025-11-26 15:05:54 -0600

    feat: action and cancel buttons

--- gymiq-app/app/components/Button.jsx
+++ gymiq-app/app/components/Button.jsx
@@ -1,7 +1,7 @@
 import { Pressable, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export default  function Button({ title, onPress, flex }) {
+export default function Button({ title, onPress, flex }) {
   return (
     <Pressable
       onPress={onPress}
@@ -22,6 +22,28 @@ export function BackButton() {
   return (
     <Pressable onPress={() => navigation.goBack()} className="px-4">
       <Text className="text-white text-[16px]">← Back</Text>
+    </Pressable>
+  );
+}
+
+export function ActionButton({ title, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      className="bg-blue-500 flex-1 py-3 mx-2 rounded-md items-center"
+    >
+      <Text className="text-slate-200 font-semibold">{title}</Text>
+    </Pressable>
+  );
+}
+
+export function CancelButton({ title, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      className="bg-slate-200 flex-1 py-3 mx-2 rounded-md items-center"
+    >
+      <Text className="font-semibold">{title}</Text>
     </Pressable>
   );
 }
