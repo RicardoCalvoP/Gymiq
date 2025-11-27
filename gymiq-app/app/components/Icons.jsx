8ba09b0eeba10fd7a8c786b6eef9c20939b56e70
RicardoCalvoP <a01028889@tec.mx>
2025-11-27 10:36:20 -0600

    feat: check icons

--- gymiq-app/app/components/Icons.jsx
+++ gymiq-app/app/components/Icons.jsx
@@ -1,4 +1,6 @@
 import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Feather from '@expo/vector-icons/Feather';
 
 export const CircleInfoIcon = (props) => (
   <FontAwesome6 name="circle-info" size={24} color="white" {...props} />
@@ -15,6 +17,15 @@ export const InfoIcon = (props) => (
 export const ChangeIcon = (props) => (
   <FontAwesome name="refresh" size={16} color="white" {...props} />
 );
+
+export const CheckIcon = (props) => (
+  <FontAwesome name="check-circle" size={24} color="green" {...props}/>
+);
+
+export const UnCheckIcon = (props) => (
+  <MaterialIcons name="radio-button-unchecked" size={24} color="white" {...props}/>
+);
+
 export default {
   CircleInfoIcon,
   HomeIcon,
