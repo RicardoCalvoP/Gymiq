abe1f69363ba2cba7863813e16846b11bf38653d
RicardoCalvoP <a01028889@tec.mx>
2025-12-03 11:32:52 -0600

    feat: added prints

--- gymiq-core/src/api_service/router_logs.py
+++ gymiq-core/src/api_service/router_logs.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 from .schemas import WorkoutLogRL
 from ..rl_engine import ingest_log_entry
-
+from pprint import pprint
+import os
 router = APIRouter(prefix="/log", tags=["logs"])
 
 
 @router.post("/")
 async def receive_log(entry: WorkoutLogRL):
-    ingest_log_entry(entry.model_dump(by_alias=False))
+    payload = entry.model_dump(by_alias=False)
+    os.system('cls')
+    print("\n===================================================== \n")
+    print("BACKEND PAYLOAD:")
+    pprint(payload, width=80, sort_dicts=False)
+    print("\n===================================================== \n")
+    ingest_log_entry(payload)
     return {"status": "ok"}
