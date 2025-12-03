867d25961729d4737faf0151e0df0c1de6313f29
RicardoCalvoP <a01028889@tec.mx>
2025-12-02 22:53:01 -0600

    feat: updated schema model to recive correct payload

--- gymiq-core/src/api_service/router_logs.py
+++ gymiq-core/src/api_service/router_logs.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
-from .schemas import WorkoutLog
+from .schemas import WorkoutLogRL
 from ..rl_engine import ingest_log_entry
 
 router = APIRouter(prefix="/log", tags=["logs"])
 
 
 @router.post("/")
-async def receive_log(entry: WorkoutLog):
-    # entry.model_dump(by_alias=False) devuelve con nombres snake_case
+async def receive_log(entry: WorkoutLogRL):
     ingest_log_entry(entry.model_dump(by_alias=False))
     return {"status": "ok"}
--- gymiq-core/src/api_service/schemas.py
+++ gymiq-core/src/api_service/schemas.py
@@ -1,21 +1,35 @@
-from typing import List
+from typing import List, Literal
 from pydantic import BaseModel, Field
 
 
-class WorkoutSet(BaseModel):
-    weight: float
+class PerfilRL(BaseModel):
+    edad: int
+    imc: float
+    peso_usuario: float
+    sexo: Literal["M", "F"]
+    historial_lesion_tipo: Literal["ninguna", "leve", "moderada", "grave"]
+    historial_lesion_tiempo_semanas: int
+    dolor_actual: Literal["no_dolor", "molestia", "dolor"]
+
+
+class WorkoutSetRL(BaseModel):
     reps: int
+    rpe: float
+    peso_kg: float
 
 
-class WorkoutExercise(BaseModel):
-    exercise_id: str = Field(alias="exerciseId")
-    sets: List[WorkoutSet]
+class WorkoutExerciseRL(BaseModel):
+    name: str
+    reps_objetivo: float
+    rpe_objetivo: float
+    sets: List[WorkoutSetRL]
 
 
-class WorkoutLog(BaseModel):
+class WorkoutLogRL(BaseModel):
     workout_id: str = Field(alias="workoutId")
-    date: str
-    exercises: List[WorkoutExercise]
+    sesion_num: int
+    perfil: PerfilRL
+    ejercicios: List[WorkoutExerciseRL]
 
     class Config:
-        populate_by_name = True  # por si luego quieres usar los nombres snake_case
+        populate_by_name = True
